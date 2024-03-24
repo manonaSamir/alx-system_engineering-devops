5570e69068252118427a0df5a6663d60d00f6ffe
manonaSamir <bena.yalla@yahoo.com>
2024-03-24 22:20:27 +0200

    install flask

--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -1,11 +1,11 @@
 # install flask from pip3
 
-package { 'flask':
-  ensure   => '2.1.0',
-  provider => 'pip3',
+
+package { 'python3-pip':
+  ensure   => installed,
 }
 
-package { 'werkzeug':
-  ensure   => '3.1.0',
-  provider => 'pip3',
+exec { 'install-flask':
+  command => 'pip3 install flask==2.1.0 werkzeug==2.1.1',
+  path => ['/usr/bin', '/usr/sbin'],
 }
