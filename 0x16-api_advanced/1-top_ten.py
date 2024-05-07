1ee838b4c588ca714aaf51c6726577b8a39b30b2
manonaSamir <bena.yalla@yahoo.com>
2024-05-08 00:03:33 +0300

    add

---
+++ 0x16-api_advanced/1-top_ten.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python3
+
+"""Gets top 10 most host posts of a subreddit using reddit api."""
+import requests
+
+
+def top_ten(subreddit):
+    """Gets top 10 most host posts of a subreddit using reddit api."""
+    headers = {'User-Agent': 'alxAPI'}
+
+    res = requests.get('https://www.reddit.com/r/{}/hot.json'
+                       .format(subreddit),
+                       headers=headers, allow_redirects=False)
+
+    if res.status_code == 200:
+        posts = res.json().get('data').get('children')
+        for post in posts[0: 10]:
+            print(post.get('data').get('title'))
+    else:
+        print(None)
