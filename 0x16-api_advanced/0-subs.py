70efc47d9fa36f8d70c9534271dc65d08d3290b1
manonaSamir <bena.yalla@yahoo.com>
2024-05-07 23:44:33 +0300

    returns the number of subscribers

---
+++ 0x16-api_advanced/0-subs.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+
+'returns the number of subscribers '
+import requests
+
+
+def number_of_subscribers(subreddit):
+    headers = {'User-Agent': 'alxAPI'}
+    res = requests.get(
+        'https://www.reddit.com/r/{}/about.json'.format(subreddit),
+        headers=headers,
+        allow_redirects=False
+    )
+
+    if res.status_code == 200:
+        return res.json()['data']['subscribers']
+    else:
+        return 0
